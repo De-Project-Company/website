bf6a3d8a210af8fd6fa48d605b88943d2889e456
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-06-02 23:48:22 +0100

    update

--- src/app/api/members/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from 'next/server';
-// import { prisma } from '@/utils/prisma';
-// import { members } from '@prisma/client';
-
-export async function POST(req: Request) {
-  await req.json();
-}
