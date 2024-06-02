fd0a4321e56a98123bb8ade4638ee6f57fd87dd9
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-06-02 14:41:45 +0100

    fix: resolve eslint warnings

--- src/app/api/members/route.ts
+++ src/app/api/members/route.ts
@@ -1,15 +1,7 @@
 // import { NextResponse } from 'next/server';
-// // import { prisma } from '@/utils/prisma';
+// import { prisma } from '@/utils/prisma';
 // import { members } from '@prisma/client';
 
-// export async function POST(req: Request) {
-//   const {
-//     fullName,
-//     bio,
-//     image,
-//     address,
-//     commetmentlevel,
-//     email,
-//     portfolio
-//   }: members = await req.json();
-// }
+export async function POST(req: Request) {
+  await req.json();
+}
